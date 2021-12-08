e30993cedaf1daf5c76fe2358078bbdc10e90157
Alex0622 <aleximora2206@gmail.com>
2021-12-08 17:03:37 -0600

    Add get MOTD function

---
+++ Example files/GetServerMOTD-example.py
@@ -0,0 +1,37 @@
+import aiohttp
+import asyncio
+import json
+
+api_token = "Insert your API token here"
+serverID = "Insert your server ID here"
+base_url = "https://api.exaroton.com/v1"
+
+
+
+async def main():
+    resp = await api_request(f"/server/{serverID}/options/motd/", "GET", {})
+    print(json.dumps(resp))
+
+
+async def api_request(path, method, data):
+    headers = {'Authorization': 'Bearer ' + api_token}
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.request(method=method, url=base_url + path, data=data) as response:
+            return await response.json()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+'''
+Get the MOTD of your server
+
+Example response:
+{
+    "success": true, 
+    "error": null, 
+    "data": {
+        "motd": "Hello World!"
+    }
+}
+'''
